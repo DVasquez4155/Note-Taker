a0cceb06bf852862a55d093b60339b91bacccb85
Daniel V <main@dvasquez4155.com>
2020-03-10 13:27:07 -0700

    Added writting and deletting of the database

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = 80;
+var bodyParser = require('body-parser');
 
 var htmlPath = path.join(__dirname, 'public');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(htmlPath));
 app.get("/notes",function(req,res) {
     res.sendfile(path.join(htmlPath, 'notes.html'))
@@ -13,14 +18,31 @@ app.get("/api/notes", async function(req,res) {
     res.json(await getDB());
 })
 app.post("/api/notes", async function(req,res) {
-    const data = req.body;
-    await writeDB(data)
-    res.send("Done");
+    const database = await getDB();
+    req.body.id = database[database.length - 1].id + 1;
+    database.push(req.body);
+    await writeDB(database)
+    res.send(req.body)
+})
+app.delete("/api/notes/:id", async function(req,res) {
+    const database = await getDB();
+    database.forEach(element => {
+        if (element.id == parseInt(req.params.id)) {
+            
+            var index = database.indexOf(element);
+            if (index > -1) {
+                database.splice(index, 1);
+            }
+        }
+    });
+    writeDB(database)
+    res.send(req.body);
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 async function getDB() {
     return JSON.parse(fs.readFileSync('./db/db.json'));
 }
 async function writeDB(data) {
-    console.log(data)
+    fs.writeFileSync('./db/db.json', JSON.stringify(data));
 }
